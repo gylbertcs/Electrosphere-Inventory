9ab9e907afd5ceac88fe2753139c9e22e84964c4
Diva Anggreini Harahap <115909849+divaaharahap@users.noreply.github.com>
2025-03-05 22:17:47 +0700

    Update test.dart

--- test.dart
+++ test.dart
@@ -1,1 +1,19 @@
+import 'dart:math';
 
+void main() {
+  print('Hello, Dart!');
+  
+  int a = 10;
+  int b = 20;
+  print('Sum: \${add(a, b)}');
+  
+  List<int> numbers = List.generate(5, (index) => Random().nextInt(100));
+  print('Random numbers: \$numbers');
+  
+  numbers.sort();
+  print('Sorted numbers: \$numbers');
+}
+
+int add(int x, int y) {
+  return x + y;
+}
